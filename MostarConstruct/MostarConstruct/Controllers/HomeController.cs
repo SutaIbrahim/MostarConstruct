14179e5aba2cea80e3c5e80ec5877adab75c58cc
Nisvet Mujkic <nisvet.mujkic@edu.fit.ba>
2017-12-19 15:11:47 +0100

    GitIgnore

--- MostarConstruct/MostarConstruct/Controllers/HomeController.cs
+++ MostarConstruct/MostarConstruct/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MostarConstruct.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your aplication description page.";
 
             return View();
         }
