12dbf3ed5f9782f5933927ca4b839c0ab42d47fc
Mirza Medar <mirza.medar@edu.fit.ba>
2017-12-28 15:19:44 +0100

    Implementiran helper za logiranje aktivnosti.

--- MostarConstruct/MostarConstruct/Controllers/HomeController.cs
+++ MostarConstruct/MostarConstruct/Controllers/HomeController.cs
@@ -4,21 +4,29 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MostarConstruct.Data;
 using MostarConstruct.Models;
+using MostarConstruct.Web.Helper;
 
 namespace MostarConstruct.Controllers
 {
     public class HomeController : Controller
     {
+        private DatabaseContext context;
+        public HomeController(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+            
         }
 
         public IActionResult About()
         {
             ViewData["Message"] = "Your aplication description page.";
-
             return View();
         }
 
@@ -32,7 +40,12 @@ namespace MostarConstruct.Controllers
         {
             return "Ja sam pozvan ajaxom!";
         }
-
+        public IActionResult Testirajlogiranje()
+        {
+            LogiranjeAktivnosti logiranje = new LogiranjeAktivnosti(context);
+            logiranje.Logiraj(2, DateTime.Now.ToLocalTime(), Request.HttpContext.Connection.RemoteIpAddress.ToString(), Request.Headers["User-Agent"].ToString().Substring(0,100), "Dodavanje", "Korisnici");
+            return View("Index");
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
