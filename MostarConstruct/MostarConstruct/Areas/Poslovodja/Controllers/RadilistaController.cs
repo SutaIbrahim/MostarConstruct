91f3d95a5d73ba20333aba3d8b0f25136a075251
Ibrahim Šuta <suta.ibrahim@edu.fit.ba>
2018-01-01 20:49:46 +0100

    [Poslovodja] Dodana 2 controllera

---
+++ MostarConstruct/MostarConstruct/Areas/Poslovodja/Controllers/RadilistaController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MostarConstruct.Data;
+using MostarConstruct.Models;
+using MostarConstruct.Web.Helper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MostarConstruct.Web.Areas.Poslovodja.Controllers
+{
+
+    [Area("Poslovodja")]
+
+    public class RadilistaController : Controller
+    {
+
+
+        private DatabaseContext db;
+
+        public IActionResult Index()
+        {
+
+            return View(db.Radilista.Include(x=>x.Projekt));
+        }
+
+
+
+        public IActionResult Dodaj()
+        {
+
+
+            return View(new Radiliste());
+        }
+
+
+
+
+        public IActionResult Dodaj(Radiliste rad)
+        {
+
+
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MostarConstruct/MostarConstruct/Areas/Poslovodja/Controllers/ZaposleniciController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MostarConstruct.Data;
+using MostarConstruct.Models;
+using MostarConstruct.Web.Helper;
+
+namespace MostarConstruct.Web.Areas.Poslovodja.Controllers
+{
+    [Area("Poslovodja")]
+    public class ZaposleniciController : Controller
+    {
+
+        private DatabaseContext db;
+
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+
+
+        public IActionResult Dodaj()
+        {
+            return View(new Drzava());
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Dodaj(Radnik radnik)
+        {
+            if (!ModelState.IsValid)
+                return View(radnik);
+
+            db.Radnici.Add(radnik);
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
