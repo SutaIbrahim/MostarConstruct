ccd60b3963fce60adfa09be797afa75bda7ed42f
Mirza Medar <mirza.medar@edu.fit.ba>
2018-02-15 00:17:09 +0100

    Izdavanje uvjerenja v1

---
+++ MostarConstruct/webapp/MostarConstruct/Areas/ClanUprave/Controllers/UvjerenjaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MostarConstruct.Data;
+using MostarConstruct.Web.Helper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MostarConstruct.Web.ViewModels;
+using MostarConstruct.Web.Areas.ClanUprave.ViewModels;
+
+namespace MostarConstruct.Web.Areas.ClanUprave.Controllers
+{
+    [Autorizacija(false, TipKorisnika.ClanUprave)]
+
+    [Area("ClanUprave")]
+    public class UvjerenjaController : Controller
+    {
+        private DatabaseContext _db;
+        public UvjerenjaController(DatabaseContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dodaj()
+        {
+            UvjerenjaDodajVM Model = new UvjerenjaDodajVM();
+            Model.listaRadnika = new List<SelectListItem>();
+            Model.listaRadnika = _db.Radnici.Select(x => new SelectListItem
+            {
+                Value = x.RadnikID.ToString(),
+                Text = x.Osoba.Ime + " " + x.Osoba.Prezime
+            }).ToList();
+            
+            return View(Model);
+        }
+    }
+}
---
+++ MostarConstruct/webapp/MostarConstruct/Areas/ClanUprave/ViewModels/UvjerenjaDodajVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostarConstruct.Web.Areas.ClanUprave.ViewModels
+{
+    public class UvjerenjaDodajVM
+    {
+        public int UvjerenjeId { get; set; }
+        public int RadnikId { get; set; }
+        public List<SelectListItem> listaRadnika { get; set; }
+        public string Svrha { get; set; }
+        public string Napomena { get; set; }
+    }
+}
