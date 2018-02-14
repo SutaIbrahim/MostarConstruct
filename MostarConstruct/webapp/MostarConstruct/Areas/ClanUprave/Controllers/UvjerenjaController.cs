c7a55325fa54cdc6b72817a9c26cc173ca7c89eb
Mirza Medar <mirza.medar@edu.fit.ba>
2018-02-15 00:42:37 +0100

    Izdavanje uvjerenja v2

--- MostarConstruct/webapp/MostarConstruct/Areas/ClanUprave/Controllers/UvjerenjaController.cs
+++ MostarConstruct/webapp/MostarConstruct/Areas/ClanUprave/Controllers/UvjerenjaController.cs
@@ -8,6 +8,9 @@ using MostarConstruct.Web.Helper;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MostarConstruct.Web.ViewModels;
 using MostarConstruct.Web.Areas.ClanUprave.ViewModels;
+using MostarConstruct.Models;
+using Microsoft.AspNetCore.Http;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace MostarConstruct.Web.Areas.ClanUprave.Controllers
 {
@@ -17,13 +20,24 @@ namespace MostarConstruct.Web.Areas.ClanUprave.Controllers
     public class UvjerenjaController : Controller
     {
         private DatabaseContext _db;
-        public UvjerenjaController(DatabaseContext db)
+        private IHttpContextAccessor _context;
+        public UvjerenjaController(DatabaseContext db,IHttpContextAccessor context)
         {
             _db = db;
+            _context = context;
         }
         public IActionResult Index()
         {
-            return View();
+            UvjerenjaIndexVM Model = new UvjerenjaIndexVM();
+            Model.listaUvjerenja = new List<UvjerenjaIndexVM.Row>();
+            Model.listaUvjerenja = _db.Uvjerenja.Select(x => new UvjerenjaIndexVM.Row
+            {
+                UvjerenjeId = x.UvjerenjeID,
+                BrojProtokola = x.BrojProtokola,
+                DatumIzdavanja = x.DatumIzdavanja,
+                Radnik = x.Radnik.Osoba.Ime + " " + x.Radnik.Osoba.Prezime
+            }).ToList();
+            return View(Model);
         }
         public IActionResult Dodaj()
         {
@@ -36,6 +50,24 @@ namespace MostarConstruct.Web.Areas.ClanUprave.Controllers
             }).ToList();
             
             return View(Model);
+        }
+        public IActionResult Snimi(UvjerenjaDodajVM model)
+        {
+            Korisnik korisnik = _context.HttpContext.Session.GetJson<Korisnik>(Konfiguracija.LogiraniKorisnik);
+            Uvjerenje novo = new Uvjerenje
+            {
+                BrojProtokola = _db.Uvjerenja.Count().ToString() + "/" + (100 + _db.Uvjerenja.Count()).ToString(),
+                DatumIzdavanja = DateTime.Now,
+                RadnikID = model.RadnikId,
+                Napomena = model.Napomena,
+                Svrha = model.Svrha,
+                ClanUpraveID = korisnik.KorisnikID
+            };
+            _db.Uvjerenja.Add(novo);
+            _db.SaveChanges();
+            LogiranjeAktivnosti logiranje = new LogiranjeAktivnosti(_db);
+            logiranje.Logiraj(korisnik.KorisnikID, DateTime.Now, _context.HttpContext.Connection.RemoteIpAddress.ToString(), _context.HttpContext.Request.Headers["User-Agent"].ToString().Substring(0, 100), "Dodavanje uvjerenja", "Uvjerenja");
+            return RedirectToAction("Index");
         }
     }
 }
